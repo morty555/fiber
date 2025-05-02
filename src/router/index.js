8c752fb4067184f9dc1798d2d963484bb8d4043f
Cookcoups <cookcoups@qq.com>
2025-05-02 18:58:57 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from '@/App.vue' // 主页
+import MainLayout from '@/views/MainLayout.vue'
 import LoginPage from '../views/LoginPage.vue'
-
+import GuideView from '@/views/GuideView.vue'
+import AnalysisView from '@/views/AnalysisView.vue'
+import MoreView from '@/views/MoreView.vue'
+import FiberView from '@/views/FiberView.vue'
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: App
-  },
+    component: MainLayout,
+    children: [
+      {
+        path: '/',
+        redirect: '/guide' // 默认重定向到指南页
+      },
+      {
+        path: 'guide',
+        name: 'guide',
+        component: GuideView
+      },
+      {
+        path: 'fiber',
+        name: 'fiber',
+        component: FiberView
+      },
+      {
+        path: 'analysis',
+        name: 'analysis',
+        component: AnalysisView
+      },
+      {
+        path: 'more',
+        name: 'more',
+        component: MoreView
+      }
+    ]
+  }
+  ,
   {
     path: '/login',
     name: 'login',
-    component: LoginPage
-  },
+    component: LoginPage,
 
+  },
 ]
 
 const router = createRouter({
